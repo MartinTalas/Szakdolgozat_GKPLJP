d885e706d9a4a4fcba510635f87724bcbf867f7c
MartinTalas <70484866+MartinTalas@users.noreply.github.com>
2022-01-25 13:25:23 +0100

    initial commit

---
+++ Assets/Scripts/Achievement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Achievement
+{
+    //---[ Constructors ]---
+    public Achievement() { }
+    public Achievement(int id_arg, string title_arg, string description_arg)
+    {
+        this.ID = id_arg;
+        this.Title = title_arg;
+        this.Description = description_arg;
+    }
+
+    //---[ Private variables ]---
+    private int id;
+    private string title;
+    private string description;
+    //image?
+
+    //---[ Getters - Setters]---
+    public int ID { get { return this.id; } set { this.id = value; } }
+    public string Title { get { return this.title; } set { this.title = value; } }
+    public string Description { get { return this.description; } set { this.description = value; } }
+}
---
+++ Assets/Scripts/DataBaseManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class DataBaseManager
+{
+    //---[Variables]---
+    private string database_name;
+
+    //---[Constructor]---
+    DataBaseManager()
+    {
+
+    }
+
+    //---[Functions]---
+}
