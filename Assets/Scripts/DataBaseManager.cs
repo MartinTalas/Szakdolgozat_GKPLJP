1201dde572256e949fbe354be887440aaa46c92d
MartinTalas <70484866+MartinTalas@users.noreply.github.com>
2022-01-26 18:54:18 +0100

    Firebase connection
    No connection with the database, but no exception in class DataBaseManager

--- Assets/Scripts/DataBaseManager.cs
+++ Assets/Scripts/DataBaseManager.cs
@@ -1,16 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Firebase;
+using Firebase.Extensions;
 
-public class DataBaseManager
+public class DataBaseManager : MonoBehaviour
 {
-    //---[Variables]---
-    private string database_name;
+    public UnityEvent on_firebase_initialized = new UnityEvent();
+    Color _color = Color.blue;
 
-    //---[Constructor]---
-    DataBaseManager()
+    void Start()
     {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread( task =>
+        {
+            if(task.Exception != null)
+            {
+                Debug.LogError("Failed initialize firebase with " + task.Exception.Message);
+                _color = Color.red;
+                return;
+            }
+            else
+            {
+                _color = Color.green;
+            }
 
+            on_firebase_initialized.Invoke();
+        });
     }
 
-    //---[Functions]---
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void getColor()
+    {
+        GetComponent<Renderer>().material.color = _color;
+    }
 }
