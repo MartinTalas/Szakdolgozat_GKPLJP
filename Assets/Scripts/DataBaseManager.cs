5bac6fddbd67068f0aeb126241fd919b1afc0f83
Tálas Martin <70484866+MartinTalas@users.noreply.github.com>
2022-02-20 18:59:04 +0100

    save

--- Assets/Scripts/DataBaseManager.cs
+++ Assets/Scripts/DataBaseManager.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Firebase;
 using Firebase.Database;
 using Firebase.Extensions;
-using System;
-using System.Threading.Tasks;
 
 public sealed class DataBaseManager
 {
