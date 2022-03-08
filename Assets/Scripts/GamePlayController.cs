c0db33c54f6b0cb49b25fff8b93a248c700ce0c1
Tálas Martin <70484866+MartinTalas@users.noreply.github.com>
2022-03-08 09:11:25 +0100

    save

---
+++ Assets/Scripts/GamePlayController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct FullUser
+{
+    Player player;
+    //Avatar avatar;
+}
+public class GamePlayController : MonoBehaviour
+{
+    List<FullUser> players = new List<FullUser>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerLimitChecker();
+    }
+
+    void playerLimitChecker()
+    {
+        if(players.Count > 8)
+        {
+            //TODO: FULL
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,6 +24,7 @@ public class Player
     private string password;
     private int score;
     private List<int> achievements; // by achievement id
+    private bool isSpeaker;
 
     //--------------------------------------------------------------------------------------------------------------------------------------------------------------
     //-----------------------------------------------------------------[ GETTER / SETTER FUNCTIONS ]----------------------------------------------------------------
@@ -33,6 +34,7 @@ public class Player
     public string Password { get { return this.password; } set { this.password = value; } }  
     public int Score { get { return this.score; } set { this.score = value; } }
     public List<int> Achievements { get { return this.achievements; } set { this.achievements = value; } }
+    public bool IsSpeaker { get { return this.isSpeaker; } set { this.isSpeaker = value; } }
 
     //--------------------------------------------------------------------------------------------------------------------------------------------------------------
 }
