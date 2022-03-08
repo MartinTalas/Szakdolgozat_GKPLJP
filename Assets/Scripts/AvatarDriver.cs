7c286212c22a81e028d9745e6c72f9011e0c019e
Tálas Martin <70484866+MartinTalas@users.noreply.github.com>
2022-03-08 16:27:45 +0100

    save
    Mostly comments and library stuff

--- Assets/Scripts/AvatarDriver.cs
+++ Assets/Scripts/AvatarDriver.cs
@@ -5,17 +5,20 @@ using UnityEngine.UI;
 
 public class AvatarDriver : MonoBehaviour
 {
+    //temporapy avatar list (for singleton AvatarManager)
     public GameObject[] casual_females = new GameObject[8];
     public GameObject[] casual_males = new GameObject[8];
     public GameObject[] elegant_females = new GameObject[8];
     public GameObject[] elegant_males = new GameObject[8];
 
+    //avatar for first load!!! [Casual female]
     public GameObject firstLoader;
 
     AvatarManager avatar_manager;
     // Start is called before the first frame update
     void Start()
     {
+        //build singleton (sealed) AvatarManager class
         avatar_manager = AvatarManager.Instance;
         avatar_manager.buildLists(casual_females, casual_males, elegant_females, elegant_males);
     }
@@ -26,7 +29,7 @@ public class AvatarDriver : MonoBehaviour
         
     }
 
-
+    //click event for next avatar
     public void toRight()
     {
         firstLoader.SetActive(false);
@@ -34,6 +37,7 @@ public class AvatarDriver : MonoBehaviour
         avatar_manager.setPreview();
     }
 
+    //click event for previous avatar
     public void toLeft()
     {
         firstLoader.SetActive(false);
@@ -41,6 +45,7 @@ public class AvatarDriver : MonoBehaviour
         avatar_manager.setPreview();
     }
 
+    //set sex ("gender") property
     public void setSexProperty()
     {
         bool sex = avatar_manager.getProperties()[0], outfit = avatar_manager.getProperties()[1];
@@ -59,6 +64,8 @@ public class AvatarDriver : MonoBehaviour
         avatar_manager.setProperties(sex, outfit);
         avatar_manager.setPreview();
     }
+
+    //set outfit property
     public void setOutfitProperty()
     {
         bool sex = avatar_manager.getProperties()[0], outfit = avatar_manager.getProperties()[1];
@@ -79,6 +86,7 @@ public class AvatarDriver : MonoBehaviour
         avatar_manager.setPreview();
     }
 
+    //Ok button: load the room scene!!!!
     public void ok()
     {
 
--- Assets/Scripts/AvatarManager.cs
+++ Assets/Scripts/AvatarManager.cs
@@ -8,19 +8,21 @@ public sealed class AvatarManager
 {
 
 
-    GameObject current_avatar;
-
+    GameObject current_avatar; // the current avatar
+    
+    //all avatars
     public GameObject[] casual_females = new GameObject[8];
     public GameObject[] casual_males = new GameObject[8];
     public GameObject[] elegant_females = new GameObject[8];
     public GameObject[] elegant_males = new GameObject[8];
 
+    //actual avatar list (by gender and outfit)
     public GameObject[] actual_avatar_set = new GameObject[8];
 
-    private int current_avatar_index = 0; //avatar counter 
+    private int current_avatar_index = 0; //current avatar index 
 
-    public Vector3 selector_central_position;
-    public Vector3 default_rotation;
+    public Vector3 selector_central_position;// default position
+    public Vector3 default_rotation; // default rotation: 180 
 
     private bool sex = false;// false = female | true = male
     private bool outfit = false;// false = casual | true = elegant
@@ -36,9 +38,11 @@ public sealed class AvatarManager
         selector_central_position = new Vector3(0.5f, 0.81f, 3.03f);
         default_rotation = new Vector3(0, 180, 0);
 
+        //add tag for the memory saving
         AddTag("Clone");
     }
 
+    //"add tag" method for GameObject 
     void AddTag(string tag)
     {
         UnityEngine.Object[] asset = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset");
@@ -65,7 +69,7 @@ public sealed class AvatarManager
 
     //--------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    //set actual avatar set
+    //set actual avatar set (outfit and sex ("gender"))
     GameObject[] setActualAvatarSet(bool actual_sex, bool actual_outfit)
     {
 
@@ -96,7 +100,7 @@ public sealed class AvatarManager
         }
     }
 
-    //change current avatar
+    //change current avatar by navigation buttons
     public void currentAvatar(bool move)
     {
         if (move)
@@ -130,12 +134,14 @@ public sealed class AvatarManager
     //load the avatar lists
     public void buildLists(GameObject[] arg1, GameObject[] arg2, GameObject[] arg3, GameObject[] arg4) 
     {
+        //get all avatars
         casual_females = arg1;
         casual_males = arg2;
         elegant_females = arg3;
         elegant_males = arg4;
         actual_avatar_set = this.setActualAvatarSet(false, false);
 
+        //set avatar positions & rotations
         for(int i = 0; i < actual_avatar_set.Length; i++)
         {
             actual_avatar_set[i].transform.rotation = UnityEngine.Quaternion.Euler(default_rotation);
@@ -154,7 +160,10 @@ public sealed class AvatarManager
             elegant_males[i].transform.position = selector_central_position;
         }
 
+        //set current avatar firtly
         current_avatar = actual_avatar_set[0];
+        //I do not wanted to instanticate the current avatar because it runs 8 times!!! It would cause absolute chaos! I used firstly burnt prefab
+        
     }
 
     //set sex and outfit and change the actual avatar list
@@ -169,16 +178,19 @@ public sealed class AvatarManager
         current_avatar = actual_avatar_set[current_avatar_index];
         current_avatar.tag = "Clone";
 
+        //destroy clones for memory saving
         var clones = GameObject.FindGameObjectsWithTag("Clone");
         foreach (var clone in clones)
         {
             MonoBehaviour.Destroy(clone);
         }
         
+        //set current active and "generate"
         current_avatar.SetActive(true);
         MonoBehaviour.Instantiate(current_avatar, current_avatar.transform.position, current_avatar.transform.rotation); 
     }
 
+    //get avatar properties
     public bool[] getProperties()
     {
         bool[] result = { sex, outfit }; 
