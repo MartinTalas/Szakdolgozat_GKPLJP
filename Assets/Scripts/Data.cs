acf3e19baa96e1a4fdccf0ef385620944842b860
Tálas Martin <70484866+MartinTalas@users.noreply.github.com>
2022-03-15 18:08:24 +0100

    save

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data
+{
+    public string username;
+    public int game_id;
+    public int[,,] avatar; //[1,1,1] [sex, outfit, avatarindex]
+}
