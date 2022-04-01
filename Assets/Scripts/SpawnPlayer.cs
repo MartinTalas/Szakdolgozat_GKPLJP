484c19c71542a62a005cb6f1bf2af39789d3524c
Tálas Martin <70484866+MartinTalas@users.noreply.github.com>
2022-04-02 01:02:29 +0200

    spawn

---
+++ Assets/Scripts/SpawnPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SpawnPlayer : MonoBehaviourPunCallbacks
+{
+    private GameObject spawned_player; //player who spawned
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        spawned_player = PhotonNetwork.Instantiate("Spwned player", transform.position, transform.rotation);
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        PhotonNetwork.Destroy(spawned_player);
+    }
+}
