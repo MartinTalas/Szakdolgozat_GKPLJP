37d35f479b379091b7660eda73a100ffc4bc573d
Tálas Martin <70484866+MartinTalas@users.noreply.github.com>
2022-04-08 20:21:04 +0200

    confirmation starts

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using Firebase.Database;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,8 +9,14 @@ public class GameController : MonoBehaviour
 {
     //--------------------------------------------------------------------------------------------------------------------------------------------------------------
     //------------------------------------------------------------------------[ VARIABLESS ]------------------------------------------------------------------------
-    
-    Rating rating;
+
+    private Data data;
+    private JsonParser jsonParser;
+    private Rating rating;
+    private DataBaseManager dataBaseManager;
+    private FirebaseDatabase db;
+    private string current_speaker = "ERR";
+    private int current_speaker_points = 0;
 
     //--------------------------------------------------------------------------------------------------------------------------------------------------------------
     //---------------------------------------------------------------[ INHERITED FROM MONOBEHAVIOUS ]---------------------------------------------------------------
@@ -17,6 +25,11 @@ public class GameController : MonoBehaviour
     void Start()
     {
         rating = Rating.Instance;
+        dataBaseManager = DataBaseManager.Instance;
+        jsonParser = JsonParser.Instance;
+
+        data = jsonParser.toObject<Data>("userdata.json");
+        //FirebaseDatabase.DefaultInstance.GetReference("current_speaker").Child(data.game_id).ValueChanged += currentSpeakerChanged();
     }
 
     // Update is called once per frame
@@ -76,5 +89,73 @@ public class GameController : MonoBehaviour
     }
 
     //--------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //---------------------------------------------------------------------[ DATABASE MANAGING ]--------------------------------------------------------------------
 
+    private async void confirmation()
+    {
+        if (Application.internetReachability == NetworkReachability.NotReachable)
+        {
+            Debug.Log("Error. Check internet connection!");
+        }
+        else
+        {  
+            db = dataBaseManager.getConnection();
+            try
+            {
+                int pnts = current_speaker_points + rating.getVoteRate();
+                await db.GetReference("points").Child(current_speaker).SetValueAsync(pnts);
+            }
+            catch (Exception ex)
+            {
+                Debug.Log("Exception: " + ex);
+            }
+            
+        }
+    }
+
+    private async void setCurrentSpeaker()
+    {
+        if (Application.internetReachability == NetworkReachability.NotReachable)
+        {
+            Debug.Log("Error. Check internet connection!");
+        }
+        else
+        {
+            db = dataBaseManager.getConnection();
+            try
+            {
+                await db.GetReference("current_speaker").Child(data.game_id).SetValueAsync(data.username);
+            }
+            catch (Exception ex)
+            {
+                Debug.Log("Exception: " + ex);
+            }
+
+        }
+    }
+
+    private async void getCurrentSpeaker()
+    {
+        //await;dblistener
+        current_speaker = "";
+    }
+
+
+    private async void getCurrentSpeakerPoints()
+    {
+        //await;dblistener
+        current_speaker_points = -1;
+    }
+    /*
+    private EventHandler<ValueChangedEventArgs> currentSpeakerChanged()
+    {
+        getCurrentSpeaker();
+        //return new EventHandler<ValueChangedEventArgs>();
+    }*/
+    //--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public void confirm()
+    {
+        confirmation();
+    }
 }
